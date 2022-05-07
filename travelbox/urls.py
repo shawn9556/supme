18d7c29de1e950593a5cd04dabc5b2c6958feaa9
ktgokr <38237922+ktgokr@users.noreply.github.com>
2022-05-07 22:51:35 +0900

    login&test => deletet, travelbox => box_create, CRUD => simply finish

--- travelbox/urls.py
+++ travelbox/urls.py
@@ -6,9 +6,9 @@ from travelbox import views
 app_name='travelbox'
 
 urlpatterns = [
-    path("supme/", views.index, name="travelbox"),
-    path('test/', views.test),
-    path('login/', views.login),
+    path("create/", views.create, name="create"),
     path('read/<int:post_id>/', views.read, name='read_box'),
-    path('read-all/', views.read_all, name='read_all')
+    path('read-all/', views.read_all, name='read_all'),
+    path('update/<int:post_id>/', views.box_update, name='box_update'),
+    path('delete/<int:post_id>/', views.delete, name='delete')
 ]
--- travelbox/views.py
+++ travelbox/views.py
@@ -1,44 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.http import HttpResponseRedirect
 
 from travelbox.models import Travel_box
 
 # Create your views here.
 
 
-def index(request):
+def create(request):
     if request.method == 'GET':
-        return render(request, 'travelbox/travelbox.html')
+        return render(request, 'travelbox/box_create.html')
     elif request.method =='POST':
         mk_box = Travel_box()
         # mk_box.id = request.POST['id']
         mk_box.accomdation = request.POST['accomdation']
-        # mk_box.food = request.POST['food']
-        # mk_box.activity = request.POST['activity']
-        # mk_box.sightseeing = request.POST['sightseeing']
+        mk_box.food = request.POST['food']
+        mk_box.activity = request.POST['activity']
+        mk_box.sightseeing = request.POST['sightseeing']
         mk_box.place_name = request.POST['place_name']
         mk_box.traffic = request.POST['traffic']
         mk_box.save()
     
-        return render(request, 'travelbox/travelbox.html')
-
-# def read(request):
-#     if request.method == 'GET':
-#         result = Travel_box.objects.get()
-
-#         return render(request,'travelbox/travelbox.html', result)
+        return render(request, 'travelbox/box_create.html')
 
 
     
-def test(request):
-    
-    return render(request, 'travelbox/test.html')
-
-def login(request):
-
-    return render(request, 'travelbox/login.html')
-
 def read(request, post_id):
     
     post = Travel_box.objects.get(id= post_id)
@@ -61,3 +48,38 @@ def read_all(request):
     }
 
     return render(request, 'travelbox/read_box_list.html', context)
+
+def box_update(request, post_id):
+    if request.method =='GET':
+            
+        post =Travel_box.objects.get(id = post_id)
+        context = {
+            'post' : post
+        }
+        return render(request, "travelbox/box_update.html", context)
+
+
+    elif request.method =='POST':
+        target_post = Travel_box.objects.get(id = post_id)
+        target_post.place_name = request.POST['place_name']
+        target_post.traffic = request.POST['traffic']
+        target_post.accomdation = request.POST['accomdation']
+        target_post.food = request.POST['food']
+        target_post.activity = request.POST['activity']
+        target_post.sightseeing = request.POST['sightseeing']
+        target_post.save()
+        
+        return HttpResponseRedirect('/travelbox/read-all/')
+
+
+def delete(request, post_id):
+    if request.method == "GET":
+        post = Travel_box.objects.get(id = post_id)
+        context = {
+            'post' : post
+        }
+        return render(request, 'travelbox/box_delete.html', context)
+    elif request.method == 'POST':
+        delete_post = Travel_box.objects.get(id = post_id)
+        delete_post.delete()
+        return HttpResponseRedirect('/travelbox/read-all/')
